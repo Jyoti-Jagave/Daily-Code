22d7efd72b6cc7a9f801b04bd92380b52742e347
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-02-25 20:54:11 +0530

    Function-Object

---
+++ 02_Cpp_SourceCode/Function-Object/function-object.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+// Define a function object (functor)
+class SumFunctor {
+public:
+    int n;
+    SumFunctor(int a) : n(a) {}
+
+    // Overload the operator() to
+    // make it callable
+    void operator()() const{
+        cout << n;
+    }
+    
+};
+
+int main(){
+    // Create a thread using
+    // the functor object
+    thread t (SumFunctor(3));
+
+    // Wait for the thread to 
+    // complete
+    t.join();
+    return 0;
+}
