8431240c010c463b2686d589f99b380e29799f88
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-02-27 21:45:49 +0530

    String_strcpy

---
+++ 01_C_SourceCode/String_strcpy/string_strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char src[] = "Hello";
+    char dest[20];
+
+    // Copies "Hello" to dest
+    strcpy(dest, src);
+    printf("%s", dest);
+    return 0;
+}
