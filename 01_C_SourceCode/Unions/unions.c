9f2afdbd1a8f20b1c05f986973de53daf5fb1d02
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-02-28 08:32:40 +0530

    Unions

---
+++ 01_C_SourceCode/Unions/unions.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Define a union with
+// Different data types
+
+union Student 
+{
+    int rollNo;
+    float height;
+    char firstLetter;
+
+};
+
+int main()
+{
+    // Declare a union variable
+    union Student data;
+
+    // Assign snd print the roll number
+    data.rollNo = 21;
+    printf("%d\n", data.rollNo);
+    data.height = 5.2;
+    printf("%.2f\n", data.height);
+    data.firstLetter = 'N';
+    printf("%c", data.firstLetter);
+
+    return 0;
+}
