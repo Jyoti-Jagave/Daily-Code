4f3f68778f3f7d73517d2cf12ab5f310e09b8512
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-02-28 08:11:39 +0530

    Structure_Pointer

---
+++ 01_C_SourceCode/Structure_Pointer/structure_pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Structure declaration
+struct  Point {
+    int x, y;
+};
+
+int main()
+{
+    struct Point p = {1, 2};
+
+    // ptr is a pointer to structure p
+    struct Point* ptr = &p;
+
+    // Accessing structure member using structure pointer
+    printf("%d %d", ptr->x, ptr->y);
+
+    return 0;
+}
