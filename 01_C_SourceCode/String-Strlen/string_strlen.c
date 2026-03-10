9d7cb49e3696f715eb901a7370c0d462def9daab
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-03-10 06:52:58 +0530

    String-Strlen

---
+++ 01_C_SourceCode/String-Strlen/string_strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[] = "Shree";
+
+    // Finding and printing length of string s
+    printf("%lu", strlen(s));
+
+    return 0;
+}
