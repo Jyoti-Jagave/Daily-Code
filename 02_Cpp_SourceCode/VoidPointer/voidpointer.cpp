5af69bb6d38c093dac371a872049156423cfbbce
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-03-10 23:16:04 +0530

    VoidPointer

---
+++ 02_Cpp_SourceCode/VoidPointer/voidpointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 42;
+
+    // void pointer holding address of an int
+    void* ptr = &x;
+
+    // Typecast before dereferencing
+    cout << "Value pointed by void pointer:" << *(static_cast<int*>(ptr)) << endl;
+
+    return 0;
+}
