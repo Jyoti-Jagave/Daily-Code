b9aba553e072c3ee9869d705570d3c8204bb684c
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-03-10 23:09:35 +0530

    SizeOfPointer

---
+++ 02_Cpp_SourceCode/SizeOfPointer/sizeofpointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *ptr1;
+    char *ptr2;
+
+
+    // Finding size using sizeof()
+    cout << sizeof(ptr1) << endl;
+    cout << sizeof(ptr2);
+
+    return 0;
+}
