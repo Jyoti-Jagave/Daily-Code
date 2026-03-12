8813ae84d3bc970648ee7772b4b86a4ae631fa6b
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-03-12 10:16:26 +0530

    Operation-in-string

---
+++ 02_Cpp_SourceCode/Operation-in-string/operation-in-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Initializing a string directly
+    string str = "Hello All";
+
+    // Printing the string
+    cout << str << endl;
+
+    return 0;
+}
