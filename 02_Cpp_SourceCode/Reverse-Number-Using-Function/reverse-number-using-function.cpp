f2e80c369c026129827af6fa196d13aa94762953
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-04-12 16:56:24 +0530

    Reverse-Number-Using-Function

---
+++ 02_Cpp_SourceCode/Reverse-Number-Using-Function/reverse-number-using-function.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+ 
+int reverseNumber(int num)
+{
+    int rev = 0;
+    while (num != 0)
+    {
+        int r = num % 10;
+        rev = rev * 10 + r;
+        num /= 10;
+    }
+    return rev;
+}
+ 
+int main()
+{
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+    int reversedNum = reverseNumber(num);
+    cout << "\nThe reverse of " << num << " is : " << reversedNum;
+    return 0;
+}
