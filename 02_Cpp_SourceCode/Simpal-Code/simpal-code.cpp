44572e4c06c4d7cf6678bb3aa5c5905ebde6f1a7
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-04-13 23:24:38 +0530

    Simpal-Code

---
+++ 02_Cpp_SourceCode/Simpal-Code/simpal-code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // print a message to the console
+    cout << "Hello, World!" << endl;
+
+    // Declare variables
+    int num1, num2, sum;
+
+    // Take input from the user
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+
+    // perfrom addition
+    sum = num1 + num2;
+
+    // display the result
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
