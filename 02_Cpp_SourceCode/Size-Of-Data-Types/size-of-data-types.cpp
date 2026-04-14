531f18d98bfdbf9a7097468a5aa557ceeeaeeef9
Jyoti-Jagave <jyotijagave75@gmail.com>
2026-04-14 23:56:52 +0530

    Size-Of-Data-Types

---
+++ 02_Cpp_SourceCode/Size-Of-Data-Types/size-of-data-types.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int integerType;
+    char charType;
+    float floatType;
+    double doubleType;
+
+    // the size of integer type
+    cout << "Size of int is: " << sizeof(integerType)
+         << "\n";
+
+    // the size of doubleType
+    cout << "Size of char is: " << sizeof(charType) << "\n";
+
+    // the size of charType
+    cout << "Size of float is: " << sizeof(floatType)
+         << "\n";
+
+    // the size of floatType
+    cout << "Size of double is: " << sizeof(doubleType)
+         << "\n";
+
+    return 0;
+}
